b3dbe90f778d569ad9cba60f42ee626f0323240a
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-04-10 05:52:26 +0300

    Подключение экранов к роутеру

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
 
       <v-list dense>
 
-        <v-list-item v-for="link in links" :key="link.title">
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -29,12 +29,12 @@
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-
-          <v-btn v-for="link in links" :key="link.title"><v-icon start :icon="link.icon"></v-icon>
-            {{ link.title }}
-          </v-btn>
-        </v-toolbar-items>  
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="link in links" :key="link.title" :to="link.url">
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <router-view></router-view>
