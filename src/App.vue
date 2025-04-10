275bd98f5946b69bffe7027b9fbed31492d8e3af
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-04-10 06:21:46 +0300

    Добавление ссылки на главную страницу

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
   <v-app>
     <v-navigation-drawer app v-model="drawer">
@@ -28,6 +29,11 @@
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <v-btn to="/">
+          Home
+        </v-btn>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in links" :key="link.title" :to="link.url">
