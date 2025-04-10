a7c3ec1d818e4d953a6f5696701ee9d6720eaa18
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-04-10 06:45:12 +0300

    Обработка клика на checkbox

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -7,7 +7,8 @@
                     <v-list-item v-for="order in orders" :key="order.id">
                         <template v-slot:prepend>
                             <v-list-item-action>
-                                <v-checkbox :input-value="order.done" color="primary"></v-checkbox>
+                                <v-checkbox :input-value="order.done" color="primary"
+                                    @click="markDone(order)"></v-checkbox>
                             </v-list-item-action>
                         </template>
                         <v-list-item-title>{{ order.name }}</v-list-item-title>
@@ -21,6 +22,7 @@
                     </v-list-item>
                 </v-list>
 
+
             </v-col>
         </v-row>
     </v-container>
@@ -33,14 +35,20 @@ export default {
             orders: [
                 {
                     id: "123",
-                    name: "Kostya",
-                    phone: "+7(978)000-00-05",
+                    name: "Kate",
+                    phone: "+7(978)774-57-96",
                     adId: "1",
                     done: true
                 }
             ]
-
         }
     },
+    methods: {
+        markDone(order) {
+            order.done = !order.done;
+            console.log(order.done);
+        },
+    },
+
 }
 </script>
