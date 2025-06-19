be250413b39603941b3305958f60e5575226695b
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-06-19 21:47:22 +0300

    15.4.  Защита роутера

--- src/store/ads.js
+++ src/store/ads.js
@@ -7,57 +7,58 @@ export default {
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
           id: "1",
+          userId: "1"
         },
         {
           title: "Second",
           desc: "Second Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-          id: "2",
+          id: "2"
         },
         {
           title: "Third",
-          desc: "Thitd Desc",
+          desc: "Third Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "3",
+          id: "3"
         },
         {
-          title: "Fouth",
-          desc: "Fouth Desc",
+          title: "Fourth",
+          desc: "Fourth Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "4",
-        },
-      ],
+          id: "4"
+        }
+      ]
     },
     mutations: {
-      createAd(state, payload){
-        state.ads.push(payload)
-      } 
+        createAd(state, payload){
+            state.ads.push(payload)
+        }
     },
     actions: {
-      createAd({commit},payload){
-        payload.id = Math.random()
-          commit('createAd', payload)
-        }    
+        createAd({commit},payload){
+            payload.id = Math.random()
+            commit('createAd', payload)
+        }
     },
     getters: {
       ads(state) {
         return state.ads;
       },
       promoAds(state) {
-        return state.ads.filter((ad) => {
-          return ad.promo;
-        });
+        return state.ads.filter(ad => {
+            return ad.promo
+        })
       },
       myAds(state) {
         return state.ads;
       },
-      adById(state) {
+      adByld(state) {
         return id => {
-        return state.ads.find(ad => ad.id == id)
-        }
-      }    
-    },
+          return state.ads.find(ad => ad.id == id);
+        };
+      }
+    }
   };
